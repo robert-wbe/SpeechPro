c3eb7fdf6c8d8312ab49cea9c03671a822577852
Robert Wiebe <robert.wiebe9@icloud.com>
2022-05-15 10:00:16 -0700

    Initial Commit

---
+++ Speech App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Speech App
+//
+//  Created by Robert Wiebe on 5/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Speech AppUITests/Speech_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Speech_AppUITestsLaunchTests.swift
+//  Speech AppUITests
+//
+//  Created by Robert Wiebe on 5/15/22.
+//
+
+import XCTest
+
+class Speech_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
