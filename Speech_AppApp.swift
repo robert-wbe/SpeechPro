2549cdf440312fd18afbbd9f5a0e75b827ef6e4f
robert-wbe <92825258+robert-wbe@users.noreply.github.com>
2022-10-06 17:47:46 +0200

    Add files via upload

---
+++ Speech_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speech_AppApp.swift
+//  Speech App
+//
+//  Created by Robert Wiebe on 5/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Speech_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
